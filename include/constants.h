78292a22f51c470416395ec3d5c36097fd90a172
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-01-25 20:48:12 +0000

    Added a constants.h header file.

---
+++ include/constants.h
@@ -0,0 +1,10 @@
+//
+// Created by mattw on 25/01/2021.
+//
+
+#ifndef INC_1D_GPESOLVER_CONSTANTS_H
+#define INC_1D_GPESOLVER_CONSTANTS_H
+
+#endif //INC_1D_GPESOLVER_CONSTANTS_H
+
+#define PI 3.14159265359
