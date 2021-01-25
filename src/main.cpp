8e02c7f7e217feb1013872efe65e7c9eeaca4dc1
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-01-25 21:57:52 +0000

    Changed to 1D code. Added condensate and time parameters.

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
+#include <cmath>
 #include "constants.h"
 
 int main() {
 
     /* -------------- Controlled variables -------------- */
     /* Grid parameters */
-    const int Nx =1024, Ny =1024;
-    const int Mx = Nx / 2, My = Ny / 2;
-    const int dx = 1, dy = 1;
-    const float dkx = PI / (Mx * dx), dky = PI / (My * dy);
-    double x[Nx], y[Ny];
-    double kx[Nx], ky[Ny];
+    const int Nx = 128;
+    const int Mx = Nx / 2;
+    const int dx = 1.;
+    const double dkx = PI / (Mx * dx);
+    double x[Nx];
+    double kx[Nx];
+    double V[Nx];
 
-    /* Generate 1D grids */
+    /* Generate 1D grid and harmonic trap */
     for (int i = 0; i < Nx; ++i) {
-        x[i] = (-Mx + i) * dx;
-        y[i] = (-My + i) * dy;
+        x[i] = (-Mx + double(i)) * dx;
+        V[i] = 0.5 * pow(x[i], 2);
     }
 
-    /* Generate 1D shifted k-space grids */
+    /* Generate 1D shifted k-space grid */
     for (int i = 0; i < Nx; ++i) {
         if (i < Nx / 2) {
-            kx[i] = float(i) * dkx;
-            ky[i] = float(i) * dky;
+            kx[i] = double(i) * dkx;
         }
         if (i >= Nx / 2) {
-            kx[i] = float(-Nx + i) * dkx;
-            ky[i] = float(-Ny + i) * dky;
+            kx[i] = double(-Nx + i) * dkx;
         }
     }
+
+    /* Condensate parameters */
+    double c0 = 1.;  // Interaction strength
+
+    /* Time parameters */
+    int Nt = 10000;
+    double dt = 1e-2;
+    double t = 0;
+
+    /* -------------- Initial State -------------- */
 
     return 0;
 }
