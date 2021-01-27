a139565f840ebed3652105e0c7cc1f0b57a2a573
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-01-27 20:35:48 +0000

    FFT plans and initial Gaussian state added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <cmath>
 #include "constants.h"
+#include <complex>
+#include "fftw3.h"
+
 
 int main() {
 
     /* -------------- Controlled variables -------------- */
     /* Grid parameters */
-    const int Nx = 128;
+    const int Nx = 128;  // Grid pts
     const int Mx = Nx / 2;
-    const int dx = 1.;
-    const double dkx = PI / (Mx * dx);
+    const int dx = 1.;   // Grid spacing
+    const double dkx = PI / (Mx * dx);  // k-space spacing
     double x[Nx];
     double kx[Nx];
     double V[Nx];
@@ -38,7 +41,21 @@ int main() {
     double dt = 1e-2;
     double t = 0;
 
-    /* -------------- Initial State -------------- */
+    /* -------------- Set up FFT plans -------------- */
+    fftw_plan p_forward, p_back;
+
+    std::complex<double> psi[Nx], psi_k[Nx];
+
+    p_forward = fftw_plan_dft_1d(Nx, reinterpret_cast<fftw_complex *>(psi), reinterpret_cast<fftw_complex *>(psi),
+                                 FFTW_FORWARD, FFTW_ESTIMATE);
+    p_back = fftw_plan_dft_1d(Nx, reinterpret_cast<fftw_complex *>(psi_k), reinterpret_cast<fftw_complex *>(psi),
+                              FFTW_BACKWARD, FFTW_ESTIMATE);
+
+    /* -------------- Generate initial Gaussian -------------- */
+    for (int i = 0; i < Nx; ++i) {
+        psi[i] = exp((-x[i] * x[i]) / 4);
+    };
+
 
     return 0;
 }
