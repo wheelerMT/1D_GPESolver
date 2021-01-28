5b5b9c89a1f485516928f95ecafae7ead1d0b9b2
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-01-28 13:44:08 +0000

    Switched to std::abs in noramlise function.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ double normalise(std::complex<double> *t_wfn, int t_arrayLength, double t_dx) {
   double sum = 0.;
 
   for (int i = 0; i < t_arrayLength; ++i) {
-    sum += (abs(t_wfn[i]) * abs(t_wfn[i])) * t_dx;
+    sum += (std::abs(t_wfn[i]) * std::abs(t_wfn[i])) * t_dx;
   }
 
   return sum;
