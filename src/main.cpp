9913fc1af422f93eb6539b323c5de3c9384b9791
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-01-31 00:41:59 +0000

    Changed parameter values. Added support for outputting wavefunction data to an output file in binary format.

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,13 @@
 #include <complex>
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 int main() {
 
     // -------------- Controlled variables -------------- //
     // Grid parameters //
-    const int Nx = 128; // Grid pts
+    const int Nx = 512; // Grid pts
     const int Mx = Nx / 2;
     const double dx = 1.;              // Grid spacing
     const double dkx = PI / (Mx * dx); // k-space spacing
@@ -34,11 +35,11 @@ int main() {
     }
 
     // Condensate parameters //
-    const double c0 = 1.;   // Interaction strength
-    const double N = 1000.; // Atom number
+    const double c0 = 10.;   // Interaction strength
+    const double N = 100000.; // Atom number
 
     // Time parameters //
-    const int Nt = 10000;   // Number of timesteps
+    const int Nt = 1000;   // Number of timesteps
     const double dt = 1e-2; // Timestep
     double t = 0;     // Time
 
@@ -56,24 +57,24 @@ int main() {
 
     // -------------- Generate initial Gaussian -------------- //
     for (int i = 0; i < Nx; ++i) {
-        psi[i] = sqrt(N) / Nx * exp((-x[i] * x[i]));
+        psi[i] = sqrt(N) / Nx * exp((-x[i] * x[i]) / 2000);
     }
 
     // -------------- Imaginary time evolution -------------- //
     for (int i = 0; i < Nt; ++i) {
 
         // Potential half-step evolution:
-        symplectic::potentialEvolution(psi, V, c0, dt, Nx);
+        symplectic::potentialEvolution(psi, V, c0, dt);
 
         fftw_execute(p_forward); // Forward FFT
 
         // Kinetic half-step evolution:
-        symplectic::kineticEvolution(psi_k, kx, dt, Nx);
+        symplectic::kineticEvolution(psi_k, kx, dt);
 
         fftw_execute(p_back); // Backward FFT
 
         // Potential half-step evolution:
-        symplectic::potentialEvolution(psi, V, c0, dt, Nx);
+        symplectic::potentialEvolution(psi, V, c0, dt);
 
         // Renormalise wavefunction:
         for (auto &j : psi) {
@@ -84,6 +85,12 @@ int main() {
         if (i % 100 == 0) { std::cout << "t = " << t << '\n'; }
 
         t += dt;
+    }
+
+    std::ofstream outfile("data.txt", std::ofstream::binary);
+
+    for (auto &i : psi) {
+        outfile.write((char *) &i, sizeof(std::complex<double>));
     }
 
     return 0;
